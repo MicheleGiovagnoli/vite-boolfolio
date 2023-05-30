1b3839b607fc588c7ba8c4b58433e9c4a6f255a3
michele <michele.giovagnoli1@gmail.com>
2023-05-30 17:25:13 +0200

    Milestone 5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,17 @@
     <div class="container">
         <div class="row">
 
+            <div class="col-3" v-for="project in projects">
+                <div class="card" style="width: 18rem;">
+                    <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">{{ project.content }}</p>
+                        <a href="#" class="btn btn-primary">Go</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -15,14 +26,15 @@ export default {
 
     data() {
         return {
-            projects: []
+            projects: [],
+            Url: 'http://127.0.0.1:8000'
         }
     },
     methods: {
         getProjects() {
             console.log('ciaomondo');
 
-            axios.get('http://127.0.0.1:8000/api/projects')
+            axios.get(`${this.Url}/api/projects`)
                 .then(Response => {
                     console.log(Response);
                     this.projects = Response.data.results;
