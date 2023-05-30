0195999cd855a312107052ade05b748559e5f500
michele <michele.giovagnoli1@gmail.com>
2023-05-30 17:16:22 +0200

    Milestone 4

---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        getProjects() {
+            console.log('ciaomondo');
+
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then(Response => {
+                    console.log(Response);
+                    this.projects = Response.data.results;
+                });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
+}
+</script>
